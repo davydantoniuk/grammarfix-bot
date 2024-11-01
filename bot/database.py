1c9b99c7ac412b6f95467dc78cf84598d8222ad9
Antoniuk Davyd <127057232+davydantoniuk@users.noreply.github.com>
2024-11-01 23:34:05 +0100

    bot's database

--- bot/database.py
+++ bot/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey
-from sqlalchemy.orm import sessionmaker , declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 import datetime
 
 
-engine = create_engine('sqlite:///bot_messages.db')
+os.makedirs("bot", exist_ok=True)
+engine = create_engine('sqlite:///bot/bot_messages.db')
 
 Base = declarative_base()
 
@@ -11,14 +13,14 @@ Base = declarative_base()
 class Message(Base):
     __tablename__ = 'messages'
 
-    id = Column(Integer, primary_key=True , autoincrement=True)
-    user_id = Column(Integer,nullable = False)
-    username = Column(String(50),nullable =True)
-    message_text = Column(Text,nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False)
+    username = Column(String(50), nullable=True)
+    message_text = Column(Text, nullable=False)
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
 
 
 Base.metadata.create_all(engine)
 
 
-Session = sessionmaker(bind = engine)
+Session = sessionmaker(bind=engine)
