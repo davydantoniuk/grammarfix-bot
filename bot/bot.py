ac085d3c2d857ee38fa1b79c13d5a656af240683
Antoniuk Davyd <127057232+davydantoniuk@users.noreply.github.com>
2024-11-15 19:42:58 +0100

    bot

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from database import Session, Message
 import logging
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from happytransformer import HappyTextToText, TTSettings
+import tf_keras
 
 load_dotenv()
 token = os.getenv('TELEGRAM_TOKEN')  # your token here
@@ -12,6 +14,13 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+# Initialize the HappyTextToText model from the local directory
+model_path = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), os.pardir, "t5-base-grammar-correction/")
+happy_tt = HappyTextToText("T5", model_path)
+# happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+args = TTSettings(num_beams=5, min_length=1)
 
 
 async def start(update: Update, context):
@@ -25,6 +34,9 @@ async def handle_message(update: Update, context):
     user_text = update.message.text
 
     try:
+        # Correct the user's input text using the local model
+        corrected_text = happy_tt.generate_text(
+            f"grammar: {user_text}", args=args).text
 
         session = Session()
         message = Message(
@@ -41,12 +53,12 @@ async def handle_message(update: Update, context):
     finally:
         if session:
             session.close()
-    await update.message.reply_text(f"You sent it: {user_text}")
+    await update.message.reply_text(f"*Original:*\n{user_text}", parse_mode='Markdown')
+    await update.message.reply_text(f"*Corrected:*\n{corrected_text}", parse_mode='Markdown')
 
 
 def main():
     app = ApplicationBuilder().token(token).build()
-
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(
         filters.TEXT & ~filters.COMMAND, handle_message))
